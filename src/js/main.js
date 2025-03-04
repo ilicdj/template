f2924e1f0726c148f8b4a90817ed6a4b1b5f923a
djolearn <80420514+djolearn@users.noreply.github.com>
2025-03-04 20:44:38 +0100

    Updated the template, replaced DAT.gui with tweakpane and added logic for Orthographic camera

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import Stats from 'stats.js'
-import GUI from 'lil-gui'
+import { Pane } from 'tweakpane'
 import gsap from 'gsap'
 import vertex from '../shaders/vertex.glsl'
 import fragment from '../shaders/fragment.glsl'
@@ -15,7 +15,7 @@ export default class Sketch {
     this.width = this.container.offsetWidth
     this.height = this.container.offsetHeight
 
-    this.renderer = new THREE.WebGLRenderer()
+    this.renderer = new THREE.WebGLRenderer({ antialias: true })
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
     this.renderer.setSize(this.width, this.height)
     this.renderer.setClearColor(0xeeeeee, 1)
@@ -23,6 +23,10 @@ export default class Sketch {
     this.renderer.outputEncoding = THREE.sRGBEncoding
 
     this.container.appendChild(this.renderer.domElement)
+
+
+    // let frastumSize = 1
+    // this.camera = new THREE.OrthographicCamera(frastumSize / -2, frastumSize / 2, frastumSize / 2, frastumSize / -2, -1000, 1000)
 
     this.camera = new THREE.PerspectiveCamera(
       70,
@@ -33,12 +37,13 @@ export default class Sketch {
     this.camera.position.set(0, 0, 2)
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-    // this.controls.enableDamping = true
+    this.controls.enableDamping = true
     this.time = 0
 
     this.fps()
     this.setupSettings()
     this.addObjects()
+    // this.setInitialResolution()
     this.resize()
     this.render()
   }
@@ -50,15 +55,19 @@ export default class Sketch {
   }
 
   setupSettings() {
-    let that = this
-    this.settings = {
-      progress: 0,
-      bloomStrength: 0,
-      bloomThreshold: 0,
-      bloomRadius: 0,
-    }
-    this.gui = new GUI()
-    this.gui.add(this.settings, 'progress', 0, 1, 0.01)
+    this.params = {
+      progress: 0
+    };
+
+    const pane = new Pane({
+      title: 'Parameters',
+    });
+
+    pane.addBinding(this.params, 'progress', {
+      min: 0,
+      max: 1,
+      step: 0.01,
+    });
   }
 
   getMaterial() {
@@ -69,7 +78,8 @@ export default class Sketch {
       uniforms: {
         uTime: { type: "f", value: 0 },
         uProgress: { type: 'f', value: 0 },
-        uTexture: { type: 't', value: new THREE.TextureLoader().load('./src/images/texture.jpg') }
+        uTexture: { type: 't', value: new THREE.TextureLoader().load('./src/images/texture.jpg') },
+        // uResolution: { type: "v4", value: new THREE.Vector4() },
       },
       // transparent: true,
       // wireframe: true
@@ -84,12 +94,51 @@ export default class Sketch {
     this.scene.add(this.mesh)
   }
 
+  // setInitialResolution() {
+  //   this.width = this.container.offsetWidth
+  //   this.height = this.container.offsetHeight
+
+  //   // Image aspect ratio handling
+  //   this.videoAspect = 1 / 1;
+  //   let a1, a2
+  //   if (this.height / this.width > this.videoAspect) {
+  //     a1 = (this.width / this.height) * this.videoAspect
+  //     a2 = 1
+  //   } else {
+  //     a1 = 1
+  //     a2 = (this.height / this.width) / this.videoAspect
+  //   }
+
+  //   // Ensure material exists before setting uniforms
+  //   if (this.material) {
+  //     this.material.uniforms.uResolution.value.x = this.width
+  //     this.material.uniforms.uResolution.value.y = this.height
+  //     this.material.uniforms.uResolution.value.z = a1
+  //     this.material.uniforms.uResolution.value.w = a2
+  //   }
+  // }
+
   handleResize() {
     this.width = window.innerWidth
     this.height = window.innerHeight
     this.renderer.setSize(this.width, this.height)
-
     this.camera.aspect = this.width / this.height
+
+    // Image aspect ratio handling
+    // this.videoAspect = 1 / 1;
+    // let a1, a2
+    // if (this.height / this.width > this.videoAspect) {
+    //   a1 = (this.width / this.height) * this.videoAspect
+    //   a2 = 1
+    // } else {
+    //   a1 = 1
+    //   a2 = (this.height / this.width) / this.videoAspect
+    // }
+    // this.material.uniforms.uResolution.value.x = this.width
+    // this.material.uniforms.uResolution.value.y = this.height
+    // this.material.uniforms.uResolution.value.z = a1
+    // this.material.uniforms.uResolution.value.w = a2
+
     this.camera.updateProjectionMatrix()
   }
 
@@ -103,9 +152,10 @@ export default class Sketch {
     this.controls.update()
     // this.mesh.rotation.x += 0.01
     // this.mesh.rotation.y += 0.02
+
     // Update uniforms
     this.material.uniforms.uTime.value = this.time
-    this.material.uniforms.uProgress.value = this.settings.progress
+    this.material.uniforms.uProgress.value = this.params.progress
 
     window.requestAnimationFrame(this.render.bind(this))
     this.stats.end()
--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,8 +1,12 @@
 uniform float uProgress;
 uniform float uTime;
 uniform sampler2D uTexture;
+uniform vec4 uResolution;
 varying vec2 vUv;
 void main() {
-  vec4 image = texture(uTexture, vUv);
+  // Adjust UV based on resolution - OrthographicCamera
+  // vec2 newUV = (vUv - vec2(0.5)) * uResolution.zw + vec2(0.5);
+
+  // vec4 image = texture(uTexture, vUv);
   gl_FragColor = vec4(uProgress,vUv, 1.0);
 }
